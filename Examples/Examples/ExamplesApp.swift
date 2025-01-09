f4553a8f34dd669b978732a00fd3d1c8473d4b66
Jong MIn <unib335@naver.com>
2025-01-09 15:49:36 +0900

    create example project

---
+++ Examples/Examples/ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamplesApp.swift
+//  Examples
+//
+//  Created by 이종민 on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
