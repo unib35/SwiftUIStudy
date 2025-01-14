ff5854b4383720a53e351e09350c74f4c2706d48
Jong MIn <unib335@naver.com>
2025-01-14 14:30:19 +0900

    car store, car detail page

--- ListNavDemo/ListNavDemo/Car.swift
+++ ListNavDemo/ListNavDemo/Car.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Car: Codable, Identifiable {
+struct Car: Codable, Identifiable, Hashable {
     var id: String
     var name: String
     var description: String
---
+++ ListNavDemo/ListNavDemo/CarDetail.swift
@@ -0,0 +1,38 @@
+//
+//  CarDetail.swift
+//  ListNavDemo
+//
+//  Created by 이종민 on 1/14/25.
+//
+
+import SwiftUI
+
+struct CarDetail: View {
+    let selectedCar: Car
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Car Details")) {
+                Image(selectedCar.imageName)
+                    .resizable()
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                Text(selectedCar.name)
+                    .font(.headline)
+                Text(selectedCar.description)
+                    .font(.body)
+                
+                HStack {
+                    Text("Hybrid").font(.headline)
+                    Spacer()
+                    Image(systemName: selectedCar.isHybrid ? "checkmark.circle" : "xmark.circle")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    CarDetail(selectedCar: carData[0])
+}
