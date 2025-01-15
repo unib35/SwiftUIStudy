6d32c20981fff96eace37ec704faef13319e876a
Jong MIn <unib335@naver.com>
2025-01-15 15:13:42 +0900

    custom progressViewStyle

--- ProgressViewDemo/ProgressViewDemo/ContentView.swift
+++ ProgressViewDemo/ProgressViewDemo/ContentView.swift
@@ -19,6 +19,9 @@ struct ContentView: View {
             //MacOS에서만 이용가능
                 .progressViewStyle(CircularProgressViewStyle())
             
+            ProgressView("Task Progress", value: progress, total: 100)
+                .progressViewStyle(ShadowProgressViewStyle())
+            
             ProgressView("Working...")
             Slider(value: $progress, in: 1...100, step: 0.1)
         }
@@ -30,4 +33,11 @@ struct ContentView: View {
     ContentView()
 }
 
-
+struct ShadowProgressViewStyle: ProgressViewStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        ProgressView(configuration)
+            .accentColor(.red)
+            .shadow(color: Color(red: 0, green: 0.7, blue: 0), radius: 5.0, x: 2.0, y: 2.0)
+            .progressViewStyle(LinearProgressViewStyle())
+    }
+}
