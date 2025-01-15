56691276d292b030b5354d457fb6058ee4f0b97e
Jong MIn <unib335@naver.com>
2025-01-15 15:23:21 +0900

    custom shape progress view

--- ProgressViewDemo/ProgressViewDemo/ContentView.swift
+++ ProgressViewDemo/ProgressViewDemo/ContentView.swift
@@ -22,6 +22,12 @@ struct ContentView: View {
             ProgressView("Task Progress", value: progress, total: 100)
                 .progressViewStyle(ShadowProgressViewStyle())
             
+            ProgressView("Task Progress", value: progress, total: 100)
+                .progressViewStyle(MyCustomProgressViewStyle())
+            
+            ProgressView("Task Progress", value: progress, total: 100)
+                .progressViewStyle(MyCircleProgressViewStyle())
+            
             ProgressView("Working...")
             Slider(value: $progress, in: 1...100, step: 0.1)
         }
@@ -39,5 +45,39 @@ struct ShadowProgressViewStyle: ProgressViewStyle {
             .accentColor(.red)
             .shadow(color: Color(red: 0, green: 0.7, blue: 0), radius: 5.0, x: 2.0, y: 2.0)
             .progressViewStyle(LinearProgressViewStyle())
+    }
+}
+
+struct MyCustomProgressViewStyle: ProgressViewStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        let percent = Int(configuration.fractionCompleted! * 100)
+        return Text("Task \(percent)% Completed")
+    }
+}
+
+struct MyCircleProgressView: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    
+    func path(in rect: CGRect) -> Path {
+            var path = Path()
+            path.addArc(center: CGPoint(x: rect.midX, y: rect.midY),
+                        radius: rect.width / 2, startAngle: startAngle,
+                        endAngle: endAngle, clockwise: true)
+            return path.strokedPath(.init(lineWidth: 100, dash: [5, 3],
+                                          dashPhase: 10))
+        }
+}
+
+struct MyCircleProgressViewStyle: ProgressViewStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        let degrees = configuration.fractionCompleted! * 360
+        let percent = Int(configuration.fractionCompleted! * 100)
+        return VStack {
+            MyCircleProgressView(startAngle: .degrees(1), endAngle: .degrees(degrees))
+                        .frame(width: 200, height: 200)
+                        .padding(50)
+                    Text("Task \(percent)% Complete")
+                }
     }
 }
