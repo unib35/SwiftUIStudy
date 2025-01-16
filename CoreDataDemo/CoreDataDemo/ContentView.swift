048258dcd10ee261c7957969ffbe10fd2e282507
Jong MIn <unib335@naver.com>
2025-01-16 13:22:12 +0900

    coredata demo ui

--- CoreDataDemo/CoreDataDemo/ContentView.swift
+++ CoreDataDemo/CoreDataDemo/ContentView.swift
@@ -10,65 +10,62 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
+    
+    @State var name: String = ""
+    @State var quantity: String = ""
 
     @FetchRequest(entity: Product.entity(), sortDescriptors: [])
     private var products: FetchedResults<Product>
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(products) { product in
-                    NavigationLink {
-                        Text("Item at \(product.name ?? "")")
-                    } label: {
-                        Text(product.name ?? "")
+        NavigationStack {
+            VStack {
+                TextField("Product name", text: $name)
+                TextField("Product quantity", text: $quantity)
+                
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        print(#fileID, #function, #line, "Add Button")
+                        addProduct()
+                    }, label: {
+                        Text("Add")
+                    })
+                    Spacer()
+                    Button(action: {
+                        print(#fileID, #function, #line, "Clear Button")
+                        deleteItems(offsets: IndexSet())
+                    }, label: {
+                        Text("Clear")
+                    })
+                    Spacer()
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+                List {
+                    ForEach(products) { product in
+                        HStack {
+                            Text(product.name ?? "Not Found")
+                            Spacer()
+                            Text(product.quantity ?? "Not Found")
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
+                .navigationTitle("Product Database")
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addProduct) {
-                        Label("Add Product", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+            .padding()
+            .textFieldStyle(RoundedBorderTextFieldStyle())
         }
+        
     }
 
     private func addProduct() {
-        withAnimation {
-            let newProduct = Product(context: viewContext)
-            newProduct.name = "TEST"
-            
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        print(#fileID, #function, #line, "- ")
     }
 
     private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { products[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        print(#fileID, #function, #line, "- ")
     }
 }
 
