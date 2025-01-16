8b03b36332ad13527cde8deab27f09d59272dc63
Jong MIn <unib335@naver.com>
2025-01-16 11:19:55 +0900

    coredata demo create

---
+++ CoreDataDemo/CoreDataDemo/CoreDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataDemoApp.swift
+//  CoreDataDemo
+//
+//  Created by 이종민 on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
