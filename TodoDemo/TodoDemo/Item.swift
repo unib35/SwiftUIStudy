090d35cf2201e87d8dfd5f31fdf99a0b41b63667
Jong MIn <unib335@naver.com>
2025-01-20 11:02:40 +0900

    todo demo start

---
+++ TodoDemo/TodoDemo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TodoDemo
+//
+//  Created by 이종민 on 1/20/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TodoDemo/TodoDemo/TodoDemoApp.swift
@@ -0,0 +1,32 @@
+//
+//  TodoDemoApp.swift
+//  TodoDemo
+//
+//  Created by 이종민 on 1/20/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TodoDemoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
