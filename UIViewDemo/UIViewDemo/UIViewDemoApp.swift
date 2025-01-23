87f973a4e99d683beb6450351b0a7b66c4c52832
Jong MIn <unib335@naver.com>
2025-01-23 13:23:21 +0900

    UIViewDemo init

---
+++ UIViewDemo/UIViewDemo/UIViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewDemoApp.swift
+//  UIViewDemo
+//
+//  Created by 이종민 on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct UIViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
