144e26a2ddcea0a4d0adb1cf1d6292c3d4d49b21
Jong MIn <unib335@naver.com>
2025-01-23 14:17:18 +0900

    MyImagePicker UIViewControllerRepresentable

---
+++ UIViewControllerDemo/UIViewControllerDemo/MyImagePicker.swift
@@ -0,0 +1,23 @@
+//
+//  MyImagePicker.swift
+//  UIViewControllerDemo
+//
+//  Created by 이종민 on 1/23/25.
+//
+
+import SwiftUI
+
+struct MyImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+}
+
+#Preview {
+    MyImagePicker()
+}
