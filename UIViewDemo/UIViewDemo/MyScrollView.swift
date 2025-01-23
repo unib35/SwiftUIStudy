88937a520da7dbc8bc4c7ba7c302737f81cf1c9e
Jong MIn <unib335@naver.com>
2025-01-23 14:14:43 +0900

    coordiantor

--- UIViewDemo/UIViewDemo/MyScrollView.swift
+++ UIViewDemo/UIViewDemo/MyScrollView.swift
@@ -10,14 +10,43 @@ import SwiftUI
 struct MyScrollView: UIViewRepresentable {
     var text: String
     
-    func makeUIView(context: Context) -> UIScrollView {
-        let scrollView = UIScrollView()
-        scrollView.refreshControl = UIRefreshControl()
-        let myLabel = UILabel(frame: CGRect(x:0, y: 0, width: 300, height: 50))
-        myLabel.text = text
-        scrollView.addSubview(myLabel)
-        return scrollView
+    
+    class Coordinator: NSObject, UIScrollViewDelegate {
+        var control: MyScrollView
+        
+        init(_ control: MyScrollView) {
+            self.control = control
+        }
+        
+        func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            print("View is Scrolling")
+        }
+        
+        @objc func handleRefresh(sender: UIRefreshControl) {
+            sender.endRefreshing()
+            
+        }
     }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> UIScrollView {
+            let scrollView = UIScrollView()
+            scrollView.delegate = context.coordinator
+            
+            scrollView.refreshControl = UIRefreshControl()
+            
+            scrollView.refreshControl?.addTarget(context.coordinator,
+                                                 action: #selector(Coordinator.handleRefresh),
+                                                 for: .valueChanged)
+            
+            let myLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 50))
+            myLabel.text = text
+            scrollView.addSubview(myLabel)
+            return scrollView
+        }
     
     
     
