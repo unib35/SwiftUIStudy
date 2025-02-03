926e2d4179d2daa00edb3dd98843ff9f2da60298
Jong MIn <unib335@naver.com>
2025-02-03 15:38:33 +0900

    superbase demo init

---
+++ SuperBaseDemo/SuperBaseDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuperBaseDemo
+//
+//  Created by 이종민 on 2/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuperBaseDemo/SuperBaseDemo/SuperBaseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperBaseDemoApp.swift
+//  SuperBaseDemo
+//
+//  Created by 이종민 on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SuperBaseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
